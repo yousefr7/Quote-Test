c47d56c4e67c346b08c6684fd275ca5a274ffe6d
yousefr7 <90420973+yousefr7@users.noreply.github.com>
2024-08-06 00:29:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const xBtn = document.getElementById('x');
 const newQuoteBtn = document.getElementById('new-quote');
-
-
+const loader = document.getElementById('loader');
+//loading
+function loading() {
+    quoteContainer.hidden = true;
+    loader.hidden = false;
+}
+//complete
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 let apiQuotes = [];
 //show new quote
 function newQuote() {
+    loading();
     // pick a random quote from api
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if author field is blank and replace it with 'unkown'
@@ -23,10 +33,12 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
 
     }
+    complete();
     quoteText.textContent = quote.text;
 }
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
